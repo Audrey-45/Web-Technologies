649cee8fa180e936667583d01f19790ddb58df91
Audrey-45 <audrey.david@edu.ece.fr>
2021-04-23 01:53:40 +0200

    Upper case problem

--- intihuatanaPerso.js
+++ intihuatanaPerso.js
@@ -1,5 +1,5 @@
 function intihuatanaPerso(){
-	document.getElementsByTagName("body")[0].style.backgroundImage = "url('Intihuatana.png')";
+	document.getElementsByTagName("body")[0].style.backgroundImage = "url('intihuatana.png')";
 	document.getElementById("intihuatana").style.display = "none";
 	document.getElementById("intihuatanaPerso").style.display = "block"; 
 }
