e9b242fc17d3b85f7dd10d39ac156a5e212888e1
Audrey-45 <audrey.david@edu.ece.fr>
2021-04-23 03:04:55 +0200

    test

---
+++ plazaPrincipalPer.js
@@ -0,0 +1,7 @@
+function plazaPrincipalPer(){
+	document.getElementsByTagName("body")[0].style.backgroundImage = "url('plazaPrincipal.png')";
+	document.getElementById("plazaPrincipalPerso").style.display = "none";
+	document.getElementsByTagName("body")[0].style.textAlign = "left";
+	document.getElementById("plazaPrincipalPerso2").style.display = "block"; 
+}
+		
